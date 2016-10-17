c4a1170901df5101663180685b9a7df5a86580a0
kylejohnson <kyle@solidstategroup.com>
2016-10-17 22:36:48 +0100

    Change port

--- server/index.js
+++ server/index.js
@@ -62,6 +62,6 @@ app.get('/api/:id', function (req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('express-handlebars example server listening on: 3000');
 });
